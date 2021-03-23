c598c9e4deb7092b368848dd378e19307065c803
alber <acg00161@red.ujaen.es>
2021-03-23 16:54:58 +0100

    Cambios home

---
+++ app/Views/welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<a class="weatherwidget-io" href="https://forecast7.com/es/37d78n3d78/jaen/" data-label_1="JAÉN" data-label_2="WEATHER" data-theme="weather_one" >JAÉN WEATHER</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script>
+
+
+</body>
+</html>
